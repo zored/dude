e13604e5ced3dbfd301e5c5795cf2fbe07674a22
zored <i@ro.gl>
2020-03-20 16:18:54 +0300

    [master] Compete type by value

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Guess Idea
-
 **Work In Progress.**
 
-## Info
-This IDEA plugin autocompletes names used in project.
-- Of values based on type.
-- Of type based on value name.
+## What it does?
+- Autocompletes default name by type and vice versa.
+- Defaults are taken from project names and types.
+
+
+## What IDE does?
+- Creates [index](./src/main/java/gl/ro/guess_idea/index) for all type-value pairs.
+- Uses this index in [completion](./src/main/java/gl/ro/guess_idea/completion).
+
+## What else?
+This is Kotlin Jetbrains Intellij IDEA plugin example made with [actual advices](https://www.jetbrains.org/intellij/sdk/docs/basics/basics.html) on their site.
--- src/main/java/gl/ro/guess_idea/completion/ValueCompletionProvider.kt
+++ src/main/java/gl/ro/guess_idea/completion/ValueCompletionProvider.kt
@@ -1,9 +1,12 @@
 package gl.ro.guess_idea.completion
 
+import com.goide.GoTypes
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
 import com.intellij.util.ProcessingContext
 import gl.ro.guess_idea.index.Type
 import gl.ro.guess_idea.index.Value
@@ -15,21 +18,36 @@ object ValueCompletionProvider : CompletionProvider<CompletionParameters>() {
         context: ProcessingContext,
         result: CompletionResultSet
     ) {
-        val items = valuesByType(parameters).flatMap { (key, values) ->
-            values.set.map { value ->
-                createLookupElement(
-                    key,
-                    value
-                )
-            }
-        }
-        result.addAllElements(items)
+        val position = parameters.position
+        val type = getParameterType(position) ?: return
+
+        result.addAllElements(createElements(position, type))
     }
 
-    private fun createLookupElement(key: Type, value: Value) =
-        LookupElementBuilder.create(key).appendTailText(value, true)
+    private fun getParameterType(position: PsiElement): Type? {
+        val definition = position.parent
+        if (definition.elementType != GoTypes.PARAM_DEFINITION) {
+            return null
+        }
 
-    private fun valuesByType(parameters: CompletionParameters) = ValuesByTypeIterator(parameters.position.project)
+        val declaration = definition.parent
+        if (declaration.elementType != GoTypes.PARAMETER_DECLARATION) {
+            return null
+        }
 
+        return declaration.children.firstOrNull { it.elementType == GoTypes.TYPE }?.text
+    }
 
+    private fun createElements(
+        position: PsiElement,
+        expectedType: Type
+    ): List<LookupElementBuilder> =
+        ValuesByTypeIterator(position.project)
+            .filter { (type) -> type == expectedType }
+            .flatMap { (type, values) ->
+                values.set.map { value -> createElement(type, value) }
+            }
+
+    private fun createElement(type: Type, value: Value) =
+        LookupElementBuilder.create(value).appendTailText("project name for $type", true)
 }
