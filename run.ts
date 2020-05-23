d3dc433a01393d7200c9b192fd20f0526aaaac3c
zored <i@ro.gl>
2020-05-23 21:21:44 +0300

    [feature/dude] Update run.ts

--- run.ts
+++ run.ts
@@ -1,10 +1,22 @@
 #!/usr/bin/env -S deno run --allow-write --allow-read
+import { green } from "https://deno.land/std@0.52.0/fmt/colors.ts";
 import { Args } from "https://deno.land/std/flags/mod.ts";
-import { Info, Commands, GitHooks } from "https://raw.githubusercontent.com/zored/deno/v0.0.16/mod.ts";
+import {
+  Info,
+  Commands,
+  GitHooks,
+} from "https://raw.githubusercontent.com/zored/deno/v0.0.16/mod.ts";
 
-const info = () => new Info().updateFiles(["README.md"]);
-const hooks = new GitHooks({"pre-commit": info});
+const success = (s: string) => console.log(green(s));
+const info = () => {
+  new Info().updateFiles(["README.md"]);
+  success("Updated info files");
+};
+const hooks = new GitHooks({ "pre-commit": info });
 new Commands({
   info,
-  hooks: (args: Args) => hooks.run(args),
+  hooks: (args: Args) => {
+    hooks.run(args);
+    success("Ran git hooks")
+  },
 }).runAndExit();
