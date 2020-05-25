4bbb8d92580fe2ab039f930bf5eda4fe815b332a
zored <i@ro.gl>
2020-05-25 19:32:09 +0300

    [feature/dude] Add dep-check and travis

--- run.ts
+++ run.ts
@@ -5,12 +5,19 @@ import {
   Commands,
   GitHooks,
   Runner,
-} from "https://raw.githubusercontent.com/zored/deno/v0.0.17/mod.ts";
+} from "https://raw.githubusercontent.com/zored/deno/v0.0.18/mod.ts";
+import { DepFactory } from "https://raw.githubusercontent.com/zored/deno/v0.0.18/src/data/dep-check.ts";
 
 const info = () => new Info().updateFiles(["README.md"]);
 const fmt = () => new Runner().run(`deno fmt ./run.ts`);
+const arch = () => new DepFactory().allByPath("src/main/java");
 
-const gitHooks = new GitHooks({ "pre-commit": info });
+const gitHooks = new GitHooks({
+  "pre-commit": () => {
+    info();
+    arch();
+  },
+});
 const hooks = (args: Args) => gitHooks.run(args);
 
-new Commands({ info, fmt, hooks }).runAndExit();
+new Commands({ info, fmt, hooks, arch }).runAndExit();
