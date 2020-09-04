81cf59e3d41a4bfea6930f89ef5560ac43fa0901
zored <i@ro.gl>
2020-09-04 17:49:10 +0300

    [master] Remove unused action and clean-up tests

--- src/test/java/gl/ro/dude/GoCompletionTestCase.kt
+++ src/test/java/gl/ro/dude/GoCompletionTestCase.kt
@@ -1,18 +1,16 @@
 package gl.ro.dude
 
-import com.intellij.openapi.ui.Messages
+import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiErrorElement
 import com.intellij.psi.PsiFile
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import gl.ro.dude.jetbrains.action.DebugAction
 import junitparams.JUnitParamsRunner
 import junitparams.Parameters
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.reflect.KClass
 
 @RunWith(JUnitParamsRunner::class)
 class GoCompletionTestCase : BasePlatformTestCase() {
@@ -30,21 +28,6 @@ class GoCompletionTestCase : BasePlatformTestCase() {
     }
 
     @Test
-    fun `field`() {
-        file(
-            """
-type Ticket struct {
-  ric<caret>
-}
-    """
-        )
-        assertCompletions(
-            "ricardo Person",
-            "richard Person"
-        )
-    }
-
-    @Test
     @Parameters(
         """import ric<caret>""",
         """import (
@@ -52,40 +35,34 @@ type Ticket struct {
             ric<caret>
        )"""
     )
-    fun completion(text: String) {
+    fun completeImports(text: String) {
         file(text)
-        assertCompletions(
-            "rick \"github.com/zored/rick.git/v5\"",
-            "ricky \"github.com/zored/ricky.git/v5\""
-        )
+        assertImports()
     }
 
     @Test
     @Parameters(
+        """func F(ric<caret>) {}""",
+        """func F() (ric<caret>) {}""",
+        """
+           struct S{}
+           func (s *S) F(ric<caret>) {}
+        """,
+        """
+           struct S{}
+           func (s *S) F() (ric<caret>) {}
+        """,
+        """
+type Ticket struct {
+  ric<caret>
+}
+    """,
         """var ric<caret>"""
     )
-    fun `var`(text: String) {
+    fun completeVariables(text: String) {
         file(text)
-        assertCompletions(
-            "ricardo Person",
-            "richard Person"
-        )
+        assertVariables()
     }
-
-    @Test
-    fun action() {
-        @Suppress("UnstableApiUsage") // TODO
-        Messages.setTestDialog {
-            assertTrue(it.contains("Go names"))
-            Messages.OK
-        }
-        runAction(DebugAction::class)
-    }
-
-    private fun <T : Any> runAction(actionClass: KClass<T>) = myFixture.performEditorAction(getActionId(actionClass))
-
-    private fun <T : Any> getActionId(actionClass: KClass<T>): String =
-        actionClass.java.toString().substring("class.".length)
 
     private fun file(text: String): PsiFile = myFixture.configureByText(
         "${fileIndex++}.go",
@@ -101,11 +78,14 @@ type Ticket struct {
         return file
     }
 
-    private fun assertCompletions(vararg expected: String) =
+    private fun assertCompletions(vararg expected: String) {
+        val actual = myFixture.completeBasic() ?: LookupElement.EMPTY_ARRAY
+
         assertEquals(
             expected.toList(),
-            myFixture.completeBasic().map { it.lookupString }.toList()
+            actual.map { it.lookupString }.toList()
         )
+    }
 
     private fun newDefinitions(): PsiFile =
         validGo(
@@ -122,5 +102,17 @@ var ricardo Person;
 func ShowPerson(richard Person) {}
 """
             )
+        )
+
+    private fun assertVariables() =
+        assertCompletions(
+            "ricardo Person",
+            "richard Person"
+        )
+
+    private fun assertImports() =
+        assertCompletions(
+            "rick \"github.com/zored/rick.git/v5\"",
+            "ricky \"github.com/zored/ricky.git/v5\""
         )
 }
