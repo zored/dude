b64c551235d21a7a4f41b8e50bb6d1d6a9af584a
Robert Akhmerov <r.akhmerov@corp.mail.ru>
2021-10-24 23:22:55 +0300

    [feature/limit_context] Remove gradle from project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
-import kotlinx.coroutines.internal.artificialFrame
-
 plugins {
     java
     id("org.jetbrains.intellij") version "1.1.5" // https://plugins.gradle.org/plugin/org.jetbrains.intellij
     kotlin("jvm") version "1.5.31" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm
     id("com.palantir.git-version") version "0.12.3" // https://plugins.gradle.org/plugin/com.palantir.git-version
 }
-
 
 val gitVersion: groovy.lang.Closure<*> by extra
 
@@ -25,14 +22,21 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-reflect:1.5.10"))
 }
 
+// https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#intellij-platform-based-products-of-recent-ide-versions
 intellij {
-    version.set("2021.2.2")
+    version.set("2021.2.3")
     type.set("IU")
-    plugins.set(listOf(
-        // https://plugins.jetbrains.com/plugin/9568-go/versions
-        "org.jetbrains.plugins.go:212.5284.31",
-        "java"
-    ))
+    plugins.set(
+        listOf(
+            // https://plugins.jetbrains.com/plugin/9568-go/versions
+            "org.jetbrains.plugins.go:212.5457.20",
+            "java"
+        )
+    )
+}
+
+tasks.wrapper {
+    gradleVersion = "6.6.1"
 }
 
 configure<JavaPluginConvention> {
--- notes/info.md
+++ notes/info.md
@@ -5,4 +5,4 @@ Here is some info about some IDE features.
 When you copy entity and paste it in another place IDE handles all imports for you. Here how it does it:
 - You copy text.
 - Copy handler stores transferrable data from file in clipboard. Data includes imports. (See `CopyHandler.execute`.)
-- Than paste hander
+- ...
